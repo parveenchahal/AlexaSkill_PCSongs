b403ae80c0df988d67a44e053a12f05b8c0eda1b
Parveen Chahal <mail.chahal@gmail.com>
2021-02-27 02:58:00 +0000

    changed base url for songs endpoint

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-BaseUrlForSongs = 'https://www.parveenchahal.com/pc_songs'
+BaseUrlForSongs = 'https://pcapis.com/pc_songs'
 FileDetailsUrl = BaseUrlForSongs + '/files/{id}'
 SubFoldersUrl = BaseUrlForSongs + '/folders/{id}/subfolders'
 FilesInFolder = BaseUrlForSongs + '/folders/{id}/files'
